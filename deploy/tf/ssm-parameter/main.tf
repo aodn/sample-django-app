924c9e08e89fabba58ae6ae50f63ea7392a8e037
digorgonzola <29941279+digorgonzola@users.noreply.github.com>
2023-12-10 03:09:24 +1100

    tf: remove obsolete ssm-parameter module

--- deploy/tf/ssm-parameter/main.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  params = jsondecode(data.aws_ssm_parameter.shared.value)
-}
-
-data "aws_ssm_parameter" "shared" {
-  name = var.parameter_name
-}
--- deploy/tf/ssm-parameter/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "parameter_name" {
-  type = string
-}
